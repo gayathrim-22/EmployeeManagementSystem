46e084f0f8c7c23c21131de9327e5a0d42086278
unknown <saytogayathrim@gmail.com>
2026-05-10 15:44:33 +0530

    get user & get User by id Done

--- src/main/java/com/example/practice/controller/UserController.java
+++ src/main/java/com/example/practice/controller/UserController.java
@@ -4,10 +4,9 @@ import com.example.practice.data.emp.User;
 import com.example.practice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
@@ -20,6 +19,22 @@ public class UserController {
     public ResponseEntity<String> createUser(@RequestBody User user) {
         userService.saveUser(user);
         return ResponseEntity.ok("User created successfully");
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    // ✅ GET - fetch user by ID
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User user = userService.getUserById(id);
+        if (user != null) {
+            return ResponseEntity.ok(user);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }
--- src/main/java/com/example/practice/service/UserService.java
+++ src/main/java/com/example/practice/service/UserService.java
@@ -5,6 +5,8 @@ import com.example.practice.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService {
     @Autowired
@@ -13,5 +15,14 @@ public class UserService {
     public User saveUser(User user) {
         return userRepository.save(user);
     }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
 }
 
